005af65c8a8f8b876e922745d2ba0ed6d5374b12
Simran <simran.debnath@kcl.ac.uk>
2022-10-03 00:45:58 +0100

    Raise ValueError exception

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+    num = 2
+    count = 1
     list = []
+    while count <= number_of_primes:
+        prime = True
+        for i in range(2, num):
+            if (num % i) == 0:
+                prime = False
+                break
+        if prime:
+            list.append(num)
+            count += 1
+        num += 1
+
     return list
